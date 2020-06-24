67f9cf6f3fc1e0585d14268b9691a873a8bd524a
lenasrd <lena.srd@hotmail.de>
2020-06-24 11:03:36 +0200

    Updates

---
+++ precache-manifest.e1a85db2f4d269b832642b766baf5c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bde3079d2dcb2cba2a2df915fd7512",
+    "url": "/svac-frontend/index.html"
+  },
+  {
+    "revision": "371057c82e73baf2e10a",
+    "url": "/svac-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d213d62d4e64a1af7e2e",
+    "url": "/svac-frontend/static/js/2.31581b42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/svac-frontend/static/js/2.31581b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371057c82e73baf2e10a",
+    "url": "/svac-frontend/static/js/main.8ce23579.chunk.js"
+  },
+  {
+    "revision": "a54ebbe96be8ef05839e",
+    "url": "/svac-frontend/static/js/runtime-main.14ae413e.js"
+  }
+]);
